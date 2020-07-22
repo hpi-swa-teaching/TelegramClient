8eecc94de4aff2b679dc94cd0abc6494d117b7c3
Lasklu <49564344+Lasklu@users.noreply.github.com>
2020-07-22 19:59:47 +0200

    Update coveralls.sh

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -6,7 +6,4 @@
 readonly COVERALLS_API="https://coveralls.io/api/v1/jobs"
 readonly coverage_results="/home/runner/.smalltalkCI/_builds/coveralls_results.json"
 upload_status=0
-
-if is_file "${coverage_results}"; then
-  curl -s -F json_file="@${coverage_results}" "${COVERALLS_API}" > /dev/null || upload_status=$?
-fi
+curl -s -F json_file="@${coverage_results}" "${COVERALLS_API}"
