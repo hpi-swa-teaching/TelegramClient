fcfa9a91e721c87e6089d48bbeb9a757d3b2c34e
Lasklu <49564344+Lasklu@users.noreply.github.com>
2020-07-22 19:56:10 +0200

    Update coveralls.sh

--- scripts/coveralls.sh
+++ scripts/coveralls.sh
@@ -8,13 +8,5 @@ readonly coverage_results="/home/runner/.smalltalkCI/_builds/coveralls_results.j
 upload_status=0
 
 if is_file "${coverage_results}"; then
-  print_info "Uploading coverage results to Coveralls..."
   curl -s -F json_file="@${coverage_results}" "${COVERALLS_API}" > /dev/null || upload_status=$?
-  if is_nonzero "${upload_status}"; then
-    print_error "Failed to upload coverage results (curl error code #${upload_status})"
-  else
-    print_success "Successfullydgajwdk jahwgd uploaded results to Coveralls."
-  fi
-else
-  print_info "No coverage result file was found. Skipping upload."
 fi
